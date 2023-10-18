e32055065852e4548b78d7559a2d9f9132581a9f
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-10-18 19:03:10 -0300

    Código Implementado

--- Program.cs
+++ Program.cs
@@ -5,8 +5,6 @@
 
 //An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
- 
-
 //Example 1:
 
 //Input: s = "anagram", t = "nagaram"
@@ -20,58 +18,67 @@ internal class Program {
     private static void Main(string[] args) {
 
         bool IsAnagram(string s, string t) {
-            
-            Dictionary<string, int> sLetters = new Dictionary<string, int>();
-            Dictionary<string, int> tLetters = new Dictionary<string, int>();
-            char[] charS = s.ToCharArray();
-            char[] charT = t.ToCharArray();
-           
-            
-            
-            for (int i = 0; i < charS.Length; i++) {
-                char atual = charS[i];
-                int cont = 0;
-                
-                if (sLetters.ContainsKey(atual.ToString())) {
-                    continue;
-                }
 
-                for (int j = 0; j < charS.Length; j++) {
-                    if (atual.Equals(charS[j])) {
-                        cont++;
+            char[] chars = s.ToCharArray();
+            char[] chart = t.ToCharArray();
+
+            for (int i = 0; i < chars.Length - 1; i++) {
+                for (int j = 0; j < chars.Length - i -1; j++) {
+                    if (chars[j] > chars[j+1]) {
+                        char aux = chars[j];
+                        chars[j] = chars[j+1];
+                        chars[j+1] = aux;
                     }
                 }
-                sLetters.Add(atual.ToString(),cont);
             }
 
-            for (int i = 0; i < charT.Length; i++) {
-                char atual = charT[i];
-                int cont = 0;
-
-                if (tLetters.ContainsKey(atual.ToString())) {
-                    continue;
-                }
-
-                for (int j = 0; j < charT.Length; j++) {
-                    if (atual.Equals(charT[j])) {
-                        cont++;
+            for (int i = 0; i < chart.Length - 1; i++) {
+                for (int j = 0; j < chart.Length - i - 1; j++) {
+                    if (chart[j] > chart[j+1]) {
+                        char aux = chart[j];
+                        chart[j] = chart[j+1];
+                        chart[j+1] = aux;
                     }
+
                 }
-                tLetters.Add(atual.ToString(), cont);
+
             }
 
-            foreach (var item in sLetters) {
-                foreach (var itemT in tLetters) {
-                    if(item.Equals(itemT)) {
-                        continue;
-                    }
+            int cont = 0;
+            for (int i = 0; i < chars.Length; i++) {
+                if (chars[i] != chart[i]) {
+                    break;
                 }
+                cont++;
+            }
+
+            if (cont.Equals(chars.Length)) {
+                Console.WriteLine("A palavra é anagrama");
                 return true;
             }
+
+
+
+            //foreach (char c in chars) {
+            //    Console.WriteLine(c);
+            //}
+
+            //Console.WriteLine(" ");
+
+            //foreach (char c in chart) {
+            //    Console.WriteLine(c);
+            //}
+
             return false;
+
         }
 
-        Console.WriteLine(IsAnagram("anagram", "nagajam"));
+
+        Console.WriteLine("Escreva uma palvra, para verificar se e anagrama :");
+        string palavra1 = Console.ReadLine();
+        Console.WriteLine("Escreva outra palavra : ");
+        string palavra2 = Console.ReadLine();
+        IsAnagram(palavra1, palavra2);
 
     }
 
