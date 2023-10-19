96d056ab676c4d37e7657a359fbe6268dcecf4e4
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-10-18 22:25:51 -0300

    Implementado o Hash Map

--- Program.cs
+++ Program.cs
@@ -17,53 +17,100 @@
 internal class Program {
     private static void Main(string[] args) {
 
-        bool IsAnagram(string s, string t) {
+        bool IsAnagram (string s, string t) {
 
             char[] chars = s.ToCharArray();
             char[] chart = t.ToCharArray();
+            Dictionary<char, int> DicS = new Dictionary<char, int>();
+            Dictionary<char, int> DicT= new Dictionary<char, int>();
+            
 
-            for (int i = 0; i < chars.Length - 1; i++) {
-                for (int j = 0; j < chars.Length - i -1; j++) {
-                    if (chars[j] > chars[j+1]) {
-                        char aux = chars[j];
-                        chars[j] = chars[j+1];
-                        chars[j+1] = aux;
-                    }
-                }
-            }
-
-            for (int i = 0; i < chart.Length - 1; i++) {
-                for (int j = 0; j < chart.Length - i - 1; j++) {
-                    if (chart[j] > chart[j+1]) {
-                        char aux = chart[j];
-                        chart[j] = chart[j+1];
-                        chart[j+1] = aux;
-                    }
-                }
-            }
-
-            int cont = 0;
             for (int i = 0; i < chars.Length; i++) {
-                if (chars[i] != chart[i]) {
-                    break;
+                int cont = 0;
+                char aux = chars[i];
+                for (int j = 0; j < chars.Length; j++) {
+                    if (aux.Equals(chars[j])) {
+                        cont++;
+                    }
                 }
-                cont++;
+                if (DicS.ContainsKey(aux)) {
+                    continue;
+                }
+
+                DicS.Add(aux, cont);
             }
 
-            if (cont.Equals(chars.Length)) {
+            for (int i = 0; i < chart.Length; i++) {
+                int cont = 0;
+                char aux = chart[i];
+                for (int j = 0; j < chart.Length; j++) {
+                    if (aux.Equals(chart[j])) {
+                        cont++;
+                    }
+                }
+                if (DicT.ContainsKey(aux)) {
+                    continue;
+                }
+                DicT.Add(aux, cont);
+            }
+
+            int contador = 0;
+            foreach (var i in DicS) {
+                char aux = i.Key;
+                foreach (var j in DicT) {
+                    if (i.Key.Equals(j.Key) && i.Value.Equals(j.Value)) {
+                        contador++;
+                    }
+                }
+                
+            }
+            if (contador.Equals(DicT.Count) && contador.Equals(DicS.Count)) {
                 return true;
             }
 
             return false;
+           
+
+
+            //for (int i = 0; i < chars.Length - 1; i++) {
+            //    for (int j = 0; j < chars.Length - i -1; j++) {
+            //        if (chars[j] > chars[j+1]) {
+            //            char aux = chars[j];
+            //            chars[j] = chars[j+1];
+            //            chars[j+1] = aux;
+            //        }
+            //    }
+            //}
+
+            //for (int i = 0; i < chart.Length - 1; i++) {
+            //    for (int j = 0; j < chart.Length - i - 1; j++) {
+            //        if (chart[j] > chart[j+1]) {
+            //            char aux = chart[j];
+            //            chart[j] = chart[j+1];
+            //            chart[j+1] = aux;
+            //        }
+            //    }
+            //}
+
+            //int cont = 0;
+            //for (int i = 0; i < chars.Length; i++) {
+            //    if (chars[i] != chart[i]) {
+            //        break;
+            //    }
+            //    cont++;
+            //}
+
+            //if (cont.Equals(chars.Length)) {
+            //    Console.WriteLine("deu bom");
+            //    return true;
+            //}
+
+            //return false;
 
         }
 
-
-        Console.WriteLine("Escreva uma palvra, para verificar se e anagrama :");
-        string palavra1 = Console.ReadLine();
-        Console.WriteLine("Escreva outra palavra : ");
-        string palavra2 = Console.ReadLine();
-        IsAnagram(palavra1, palavra2);
+        Console.WriteLine(IsAnagram("ab", "a"));
+        
 
     }
 
